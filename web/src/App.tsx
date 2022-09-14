226c22df5af12dbc31a87f6627c3c81b8f0a44ce
Kvojps <josebazjunior09@gmail.com>
2022-09-14 09:55:21 -0300

    finalizando tela inicial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,18 @@
 import './styles/main.css'
 
 import logoImg from './assets/logo-nlw-esports.svg'
+import {MagnifyingGlassPlus} from 'phosphor-react'
 
 function App() {
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 w-5/6">
       <img src={logoImg}/>
 
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui!
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
+      <div className="grid grid-cols-6 gap-6 mt-16">
         <a href="">
           <img src="/game-1.png" />
         </a>
@@ -34,6 +35,21 @@ function App() {
             <span className='text-zinc-300 text-sm block'>4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block" >
+              Não encontrou seu duo
+            </strong>
+            <span className="text-zinc-400" >Publique um anúncio para encontrar seu duo</span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   )
