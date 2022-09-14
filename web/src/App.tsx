819fb161ea23fd13a1ba5140eb0d90b1b8f370f0
Kvojps <josebazjunior09@gmail.com>
2022-09-14 00:35:48 -0300

    adicionando tela inicial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,40 @@
 import './styles/main.css'
 
+import logoImg from './assets/logo-nlw-esports.svg'
+
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-500">
-      
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg}/>
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui!
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href="">
+          <img src="/game-1.png" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" />
+        </a>
+        <a href="" className='relative'>
+          <img src="/game-6.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0'>
+            <strong className='font-bold text-white block'>League of legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
