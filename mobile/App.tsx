80e7540932c3fa98ae2c46b2d6da10bec60423c8
Kvojps <josebazjunior09@gmail.com>
2022-09-14 13:25:18 -0300

    adicionando home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,17 @@
-import { View } from 'react-native';
+import {StatusBar} from 'react-native';
+
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home/>
+    </Background>
   );
 }
